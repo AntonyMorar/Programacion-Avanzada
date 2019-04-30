2986dde6b1844c1cc28d09ef6a0e31e54d576265
Antony999k <lv999k@hotmail.com>
2019-04-29 23:27:30 -0500

    :sparkles: Image filters with OpenMp and Threads

---
+++ ImageFilters_H9/ImageFiltersOpenMP_H9/ppms.h
@@ -0,0 +1,106 @@
+#ifndef PPMS_H
+#define PPMS_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include <time.h>
+#include <math.h>
+#include <omp.h>
+
+// Assign constants to the colors
+typedef enum color_enum {R, G, B} color_t;
+
+// Structure to store a single pixel
+typedef struct pixel_struct {
+    // One byte: Allow max value to be 255
+    unsigned char data[3];
+    // Two bytes: Allow max value to be 65535
+    //unsigned short data[3];
+} pixel_t;
+
+// Structure for the PPM image
+typedef struct ppm_struct {
+    char magic_number[3];
+    int width;
+    int height;
+    int max_value;
+    pixel_t ** pixels;
+} ppm_t;
+
+//Struct for the kernel matrix
+typedef struct kerner_struct{
+    int size; //The kernel matrix are square, the size represent the row and column size
+    int center;
+    int ** value;
+}kernel;
+
+///// FUNCTION DECLARATIONS
+
+// Get the memory necessary to store an image of the size indicated in the structure
+void allocateMemory(ppm_t * image);
+
+// Release the memory for the image
+void freeMemory(ppm_t * image);
+
+// Get the memory necessary to store kernel matrix
+void allocateKernel(kernel * filter);
+
+// Release the memory for the kernel
+void freeKernel(kernel * filter);
+
+// Open an image file and copy its contents to the structure
+void readImageFile(char * filename, ppm_t * image);
+
+// Write the data stored in a structure into an image file
+void writeImageFile(char * filename, const ppm_t * image);
+
+// Read each of the numerical values for the color components
+//  and store them in the arrray for the pixels
+void getAsciiPixels(ppm_t * image, FILE * file_ptr);
+
+// Copy the bytes from the image into the data array for the pixels
+void getBinaryPixels(ppm_t * image, FILE * file_ptr);
+
+// Write the numerical values for the color components into
+//  the output file, giving them a nice format
+void writeAsciiPixels(const ppm_t * image, FILE * file_ptr);
+
+// Copy the pixel data into the file as bytes
+void writeBinaryPixels(const ppm_t * image, FILE * file_ptr);
+
+// Compute the negative colors of an image
+void getNegativeImage(ppm_t * image);
+
+// Print the pixels of the image as hexadecimal on the console
+void printPixels(ppm_t * image);
+
+// Rotate the image a specified number of degrees
+// Modify the structure received as a parameter
+void rotateImage(ppm_t * source, int angle);
+
+// Rotate the image and write the result in another structure
+void rotateImage90(ppm_t * destination, const ppm_t * source);
+
+// Rotate the image and write the result in another structure
+void rotateImage180(ppm_t * destination, const ppm_t * source);
+
+// Rotate the image and write the result in another structure
+void rotateImage270(ppm_t * destination, const ppm_t * source);
+
+// Scale an image by a given percentage
+// Modify the structure received as a parameter
+void scaleImage(ppm_t * source, int scale);
+
+// Apply the scale to the pixels in an image
+void resizeImage(ppm_t * destination, const ppm_t * source, int scale);
+
+//Read the kernel from the file and returns a kernel struct
+void getKernel(kernel *filter, char *fileName);
+
+//Apply a filter to mconvolution matrix image
+void filterImage(ppm_t * source, char* kernel);
+
+void filterImageP(const ppm_t * destination, ppm_t * source, char* kernelName);
+
+#endif  /* NOT PPMS_H */
---
+++ ImageFilters_H9/ImageFiltersThreads_H9/test_ppm.c
@@ -0,0 +1,136 @@
+/*
+   Sample program to test libraries.
+   Uses images in PPM format:
+    http://netpbm.sourceforge.net/doc/ppm.html
+
+    Gilberto Echeverria
+    gilecheverria@yahoo.com
+    31/01/2018
+ 
+    Editado por Antony Adrian Morales Rosas
+    lv999k@hotmail.com
+    29/Abril/2019
+*/
+
+#include <unistd.h>
+#include <getopt.h>
+#include "string_functions.h"
+#include "ppms.h"
+
+
+void usage(char * program);
+void parseOptions(int argc, char * argv[]);
+void imageOperations(char * in_filename, char * out_filename, int doNegative, int rotationAngle, int newScale, int filter, char * filterName);
+
+
+int main(int argc, char * argv []){
+    printf("\n=== PROGRAM TO TEST PPM READING AND WRITING (Threads) ===\n");
+
+    parseOptions(argc, argv);
+
+    return 0;
+}
+
+void usage(char * program){
+    printf("Usage:\n");
+    printf("\t%s -i {input_file} -o {output_file} [-n] [-r {degrees}] [-s {percentage}] [-f {filter_file}]\n", program);
+    printf("Available operations:\n");
+    printf("\t-n : Compute the negative of an image\n");
+    printf("\t-r : Rotate the image the specified degrees (90, 180, 270)\n");
+    printf("\t-s : Scale the image up or down a percentage (50, 200)\n");
+    printf("\t-f : Indicate the name of the file containing the convolution filter\n");
+    exit(EXIT_FAILURE);
+}
+
+void parseOptions(int argc, char * argv[]){
+    int opt;
+    char * in_filename = NULL;
+    char * out_filename = NULL;
+    char * filter_filename = NULL;
+    unsigned short int input_ok = 0;
+    unsigned short int output_ok = 0;
+    unsigned short int filter_ok = 0;
+    unsigned short int doNegative = 0;
+    int rotationAngle = 0;
+    unsigned int newScale = 0;
+
+    while ( (opt = getopt(argc, argv, "i:o:f:nr:s:")) != -1 ){
+        switch (opt){
+            case 'i':
+                in_filename = optarg;
+                input_ok = 1;
+                break;
+            case 'o':
+                out_filename = optarg;
+                output_ok = 1;
+                break;
+            case 'f':
+                filter_filename = optarg;
+                filter_ok = 1;
+                break;
+            case 'n':
+                doNegative = 1;
+                break;
+            case 's':
+                newScale = atoi(optarg);
+                break;
+            case 'r':
+                rotationAngle = atoi(optarg);
+                break;
+            default:
+                printf("Invalid option '%c'\n", opt);
+                usage(argv[0]);
+                break;
+        }
+
+    }
+
+    if (input_ok && output_ok){
+        // Call the function to do image operations
+        imageOperations(in_filename, out_filename, doNegative, rotationAngle, newScale, filter_ok, filter_filename);
+    }
+    else{
+        usage(argv[0]);
+    }
+}
+
+void imageOperations(char * in_filename, char * out_filename, int doNegative, int rotationAngle, int newScale, int filter, char * filterName){
+    // Create the image variables and initialize to be empty
+    ppm_t input_image = {"", 0, 0, 0, NULL};
+
+    // Load the image into the program
+    readImageFile(in_filename, &input_image);
+    printf("Read file '%s' successfully\n", in_filename);
+
+#ifdef DEBUG
+    // The file just read
+    printf("THESE ARE THE PIXELS\n");
+    printPixels(&input_image);
+#endif /* DEBUG */
+
+    if (doNegative){
+        // Transform the image into a negative
+        getNegativeImage(&input_image);
+    }
+
+    if (rotationAngle){
+        // Rotate the image
+        rotateImage(&input_image, rotationAngle);
+    }
+
+    if (newScale){
+        // Scale down the image
+        scaleImage(&input_image, newScale);
+    }
+    
+    if(filter){
+        filterImage(&input_image, filterName);
+    }
+
+    // Save the new image
+    writeImageFile(out_filename, &input_image);
+    printf("Wrote file '%s' successfully\n", out_filename);
+
+    // Liberate the memory used for the structure
+    freeMemory( &input_image );
+}
